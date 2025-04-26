fb7a3c5066709624d7c6afda77e81ee435aa06ee
Avishkar2004 <avishkarkakde2004@gmail.com>
2025-04-26 20:32:09 +0530

    Updates

---
+++ src/components/About.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "../styles/About.css";
+import FadeInSection from "./FadeInSection";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    const one = (
+      <p>
+        I am currently a <b>Full Stack Developer</b> at{" "}
+        <a href="https://techstarsolution-77061.web.app/">
+          Techstar Solution Pvt Ltd
+        </a>
+        . I have recently completed my <b>Bachelor of Science</b> in{" "}
+        <b>Computer Science</b> from
+        <a href="https://bamu.ac.in/">
+          {" "}
+          Dr. Babasaheb Ambedkar Marathwada University (BAMU)
+        </a>
+        .
+      </p>
+    );
+    const two = (
+      <p>
+        Outside of work, I'm interested in following developments in science. I
+        also play a lot of video games and occasionally enjoy reading books.
+      </p>
+    );
+
+    const tech_stack = ["Typescript", "React.js", "Javascript ES6+"];
+
+    return (
+      <div id="about">
+        <FadeInSection>
+          <div className="section-header ">
+            <span className="section-title">/ about me</span>
+          </div>
+          <div className="about-content">
+            <div className="about-description">
+              {[one]}
+              {"Here are some technologies I have been working with:"}
+              <ul className="tech-stack">
+                {tech_stack.map(function (tech_item, i) {
+                  return (
+                    <FadeInSection delay={`${i + 1}00ms`}>
+                      <li>{tech_item}</li>
+                    </FadeInSection>
+                  );
+                })}
+              </ul>
+              {[two]}
+            </div>
+            <div className="about-image">
+              <img alt="Avishkar Kakde" src={"/assets/me.jpeg"} />
+            </div>
+          </div>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Intro.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import "../styles/Intro.css";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import FadeInSection from "./FadeInSection";
+import FractalTree from "./FractalTree";
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+      visible: true,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    return (
+      <div id="intro">
+        <FractalTree></FractalTree>
+        <Typist avgTypingDelay={120}>
+          <span className="intro-title">
+            {"hi, "}
+            <span className="intro-name">{"Avi"}</span>
+            {" here."}
+          </span>
+        </Typist>
+        <FadeInSection>
+          <div className="intro-subtitle">I create stuff sometimes.</div>
+          <div className="intro-desc">
+            I'm a software engineer from Pune, India. As a Full Stack Web
+            Developer, I am driven by a passion for innovation and a keen eye
+            for detail. With a solid foundation in both frontend and backend
+            development, I bring a holistic approach to every project,
+            seamlessly integrating design and functionality to create immersive
+            digital experiences.
+          </div>
+          <a
+            href="mailto:avishkarkakde2004@gmail.com"
+            className="intro-contact"
+          >
+            <EmailRoundedIcon></EmailRoundedIcon>
+            {" Say hi!"}
+          </a>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default Intro;
